71287098368358db30401f703127bc7ee5c9beac
fanhuimin <fhm011101@126.com>
2020-05-25 15:15:36 +0800

    toggleTodo

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -3,10 +3,16 @@
     <input id="toggle-all" class="toggle-all" type="checkbox" />
     <label for="toggle-all">Mark all as complete</label>
     <ul class="todo-list">
-      <li class="todo completed editing">
+      <!-- editing completed -->
+      <li
+        class="todo"
+        :class="{completed: item.isCompleted}"
+        v-for="(item, index) in todoList"
+        :key="index"
+      >
         <div class="view">
-          <input class="toggle" type="checkbox" />
-          <label>1111</label>
+          <input class="toggle" type="checkbox" @change="toggleTodo(index)" />
+          <label>{{item.value}}</label>
           <button class="destroy"></button>
         </div>
         <input class="edit" type="text" />
@@ -15,7 +21,19 @@
   </section>
 </template>
 <script>
+import { computed } from "vue";
+import { useStore } from "vuex";
 export default {
-	
-}
+  setup() {
+    const store = useStore();
+    const todoList = computed(() => store.state.todoList);
+    const toggleTodo = index => {
+      store.commit("toggleTodo", index);
+    };
+    return {
+      todoList,
+      toggleTodo
+    };
+  }
+};
 </script>
--- src/store.js
+++ src/store.js
@@ -1,10 +1,17 @@
 import { createStore } from 'vuex'
 const state = {
-	todoList: ['123']
+	todoList: []
 }
 const mutations = {
 	addTodo(state, todo) {
-		state.todoList.push(todo)
+		var todoItem = {
+			isCompleted: false,
+			value: todo
+		}
+		state.todoList.push(todoItem)
+	},
+	toggleTodo(state, index) {
+		state.todoList[index].isCompleted = !state.todoList[index].isCompleted
 	}
 }
 export default createStore({
